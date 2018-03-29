e611d411962d919ae369c4c98f6e822e5456cee1
Yongwei Wang <npuyw.wang@gmail.com>
2018-03-28 22:41:22 -0700

    Update Prytical0_append_id.py

--- Prytical0_append_id.py
+++ Prytical0_append_id.py
@@ -7,21 +7,20 @@ Created on Wed Mar 28 22:08:48 2018
 """
 
 "default params are only created for once, not for multiple times"
-# case 1) 
+# case 1) default initialization with [], different lists point to a same id
 def append_list(num, res=[]):
     print("res addr:", id(res))
     res.append(num)
     return res
 
 list_1 = append_list(1)
-print(list_1)
+print(list_1)  # [1]
 list_2 = append_list(2)
-print(list_2)
-
+print(list_2)  # [1, 2]
 list_3 = append_list(3)
-print(list_3)
+print(list_3)  # [1, 2, 3]
 
-
+# display three lists after append operation
 print("Three lists:", list_1, list_2, list_3)
 
 """ 
@@ -31,7 +30,7 @@ res addr: 140619199188872
 Three lists: [1, 2, 3] [1, 2, 3] [1, 2, 3]
 """
 
-# case 2)
+# case 2) default initialization with None, and force to create new id if 2nd param set to be default 
 def append_list(num, res=None):
     if res is None:
         res=[]
@@ -48,7 +47,7 @@ print(list_5)
 list_6 = append_list(6)
 print(list_6)
 
-
+# display three lists after append operation
 print("Three lists:", list_3, list_4, list_5)
 """
 res addr: 140619199188040
